11652e91fd80ee9bafacfaeff895bffe7cec8570
dilosh12 <dilora753@gmail.com>
2020-08-01 15:43:23 -0400

    add BookARoom.java

---
+++ src/BookARoom.java
@@ -0,0 +1,7 @@
+public class BookARoom {
+    public static void main(String[] args) {
+        System.out.println("Please enter what kind of room you like: two queen beds or one king bed ");
+
+
+    }
+}
