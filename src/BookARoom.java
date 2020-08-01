81b53590c6d61e4f1a5906ee84e3a646be634aae
Diloro A <68902194+dilosh12@users.noreply.github.com>
2020-08-01 16:02:02 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,6 +2,6 @@ public class BookARoom {
     public static void main(String[] args) {
         System.out.println("Please enter what kind of room you like: two queen beds or one king bed ");
 
-
+        //adding two lines
     }
 }
