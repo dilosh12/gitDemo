fc90a0bcc1cee0d611dbe2e2985922234416ff1d
dilosh12 <dilora753@gmail.com>
2020-08-01 11:52:30 -0400

    update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,6 +2,12 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("Hello B20, it is my pleasure to be with you! -Asiya");
-        System.out.println("Thank you");
+
+
+        System.out.println("Today August 1st, we are learning Git");
+        int a = 1;
+        int b = 99;
+        int total = a + b;
+        System.out.println(total+"% Exited");
     }
 }
