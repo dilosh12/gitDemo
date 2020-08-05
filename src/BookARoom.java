a13831a7753def21a62c02305b00c2fc0d565526
dilosh12 <dilora753@gmail.com>
2020-08-05 09:08:27 -0400

    update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -3,5 +3,7 @@ public class BookARoom {
         System.out.println("Please enter what kind of room you like: two queen beds or one king bed ");
 
         //adding two lines
+
+        //Another day to practice
     }
 }
